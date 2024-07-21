ebfa1ac7b8390aa47ecff2c97fbdc2bfb73d42c8
neel-ladani <neelladani1010@gmail.com>
2024-07-21 17:20:25 +0200

    Add solution to string2

--- basic/string2.py
+++ basic/string2.py
@@ -16,8 +16,15 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
+  if len(s) >= 3:
+    if s[-3:] != 'ing': s = s + 'ing'
+    else: s = s + 'ly'
+  return s
+  # LAB(replace solution)
+  # return
+  # LAB(end solution)
   # +++your code here+++
-  return
+  
 
 
 # E. not_bad
@@ -29,8 +36,13 @@ def verbing(s):
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
 def not_bad(s):
+  n = s.find('not')
+  b = s.find('bad')
+  if n != -1 and b != -1 and b > n:
+    s = s[:n] + 'good' + s[b+3:]
+  return s
   # +++your code here+++
-  return
+  
 
 
 # F. front_back
@@ -41,8 +53,16 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
+   a_middle = len(a) // 2  # Use the // operator to perform integer division.
+   b_middle = len(b) // 2
+   if len(a) % 2 == 1:  # add 1 if length is odd
+    a_middle = a_middle + 1
+   if len(b) % 2 == 1:
+    b_middle = b_middle + 1
+   return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
+
   # +++your code here+++
-  return
+  
 
 
 # Simple provided test() function used in main() to print
